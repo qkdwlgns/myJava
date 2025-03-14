ae1e80659a645eee5ad161b67fc6debc17cf6607
qkdwlgns <bangjihoon12@naver.com>
2025-03-14 14:20:32 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello!! My First Java Coding!!");
+        System.out.println("안녕하세요.");
+        System.out.println("Hello!!");
+        System.out.println("bangjihun");
+        System.out.println("방지훈");
+    }
+}
