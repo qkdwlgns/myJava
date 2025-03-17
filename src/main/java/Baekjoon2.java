9ad8ccee6e15f68bfe35cb9869eada047267bf57
qkdwlgns <bangjihoon12@naver.com>
2025-03-17 10:16:56 +0900

    변경

---
+++ src/main/java/Baekjoon2.java
@@ -0,0 +1,7 @@
+public class Baekjoon2 {
+    public static void main(String[] args){
+        int a = 1, b = 2;
+        int result = a + b;
+        System.out.printf("%d + %d = %d",a,b,result);
+    }
+}
