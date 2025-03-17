515df00ce3b48b7d92e6ce7df054c0442c114cb9
qkdwlgns <bangjihoon12@naver.com>
2025-03-17 10:19:27 +0900

    변경

---
+++ src/main/java/Baekjoon4.java
@@ -0,0 +1,7 @@
+public class Baekjoon4 {
+    public static void main(String[] args){
+        int a = 4, b = 3;
+        int result = a * b;
+        System.out.printf("%d * %d = %d",a,b,result);
+    }
+}
