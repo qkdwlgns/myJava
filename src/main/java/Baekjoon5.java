1a5d109f8a09dae8ef5835374c11f08b5f5702b9
qkdwlgns <bangjihoon12@naver.com>
2025-03-17 10:21:23 +0900

    변경

---
+++ src/main/java/Baekjoon5.java
@@ -0,0 +1,7 @@
+public class Baekjoon5 {
+    public static void main(String[] args){
+        int a = 12, b = 4;
+        int result = a / b;
+        System.out.printf("%d / %d = %d",a,b,result);
+    }
+}
