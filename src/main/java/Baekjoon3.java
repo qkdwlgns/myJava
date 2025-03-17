360099fe60345e45966ee3b34139ee604453e73c
qkdwlgns <bangjihoon12@naver.com>
2025-03-17 10:18:08 +0900

    변경

---
+++ src/main/java/Baekjoon3.java
@@ -0,0 +1,7 @@
+public class Baekjoon3 {
+    public static void main(String[] args){
+        int a = 2, b = 1;
+        int result = a - b;
+        System.out.printf("%d - %d = %d",a,b,result);
+    }
+}
